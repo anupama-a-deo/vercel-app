079adecc1d02be36b3b08b88e3572b40d2d3d1d4
Anupama Deo <anupama.deo@gramener.com>
2025-01-31 16:26:40 +0530

    feat(api):updated index.py

--- api/index.py
+++ api/index.py
@@ -1,8 +1,5 @@
-# api/index.py
 import json
 from http.server import BaseHTTPRequestHandler
-    
-from flask import Flask, request, jsonify
 
 # class handler(BaseHTTPRequestHandler):
 #     def do_GET(self):
@@ -10,28 +7,27 @@ from flask import Flask, request, jsonify
 #         self.send_header('Content-type','application/json')
 #         self.end_headers()
 #         self.wfile.write(json.dumps({"message": "Hello!"}).encode('utf-8'))
+
+#         # with open(join('data', 'file.txt'), 'r') as file:
+#         #   for line in file:
+#         #     self.wfile.write(line.encode())
+#         # return
 #         return
-
-
-app = Flask(__name__)
-
-# Dummy marks data for names
-marks_data = {
-    "X": 10,
-    "Y": 20,
-    "Z": 15,  # You can add more names and marks here
-}
-
-@app.route('/api', methods=['GET'])
-def get_marks():
-    # Get the list of 'name' parameters from the query string
+    
+def handler(request):
+    # Extract the 'name' parameters from the query string
     names = request.args.getlist('name')
+    
+    marks_data = [{"name":"Qz8Y5uug","marks":18},{"name":"lKZelheI","marks":43},{"name":"MxNg3ItL","marks":32},{"name":"qaeqno","marks":20},{"name":"CMKPj","marks":8},{"name":"N3Ddd0T3","marks":81},{"name":"H2wSm19VC","marks":4},{"name":"q4LJFVI5K","marks":80},{"name":"8","marks":93},{"name":"VHlVPSTZ","marks":91},{"name":"ATo0CG","marks":70},{"name":"hRHf","marks":46},{"name":"NUbNnm36F","marks":79},{"name":"SfQ9jJN","marks":48},{"name":"O9j","marks":41}]
     
     # Get the corresponding marks for the names
     marks = [marks_data.get(name, None) for name in names]  # None for names not found
     
-    # Return the response in JSON format
-    return jsonify({"marks": marks})
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    # Return the response as JSON
+    return {
+        'statusCode': 200,
+        'body': json.dumps({"marks": marks}),
+        'headers': {
+            'Content-Type': 'application/json',
+        }
+    }
